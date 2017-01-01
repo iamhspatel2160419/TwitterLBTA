31ac3a8de387f25b6ae3de02dca2eb6023eff36b
Brian Voong <bhlvoong@gmail.com>
2017-01-01 13:53:57 -0800

    Ep 3 cell subview layout

--- TwitterLBTA/Cells.swift
+++ TwitterLBTA/Cells.swift
@@ -26,21 +26,68 @@ class UserCell: DatasourceCell {
     
     override var datasourceItem: Any? {
         didSet {
-            nameLabel.text = datasourceItem as? String
+//            nameLabel.text = datasourceItem as? String
         }
     }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "TEST TEST TEST"
+        label.backgroundColor = .green
         return label
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "username"
+        label.backgroundColor = .purple
+        return label
+    }()
+    
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .yellow
+        return textView
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        return button
     }()
     
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .yellow
         
+        addSubview(profileImageView)
         addSubview(nameLabel)
-        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
+        
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        
+        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
     }
 }
+
+
+
+
+
+
+
+
+
--- TwitterLBTA/HomeDatasourceController.swift
+++ TwitterLBTA/HomeDatasourceController.swift
@@ -16,6 +16,10 @@ class HomeDatasourceController: DatasourceController {
         self.datasource = homeDatasource
     }
     
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
